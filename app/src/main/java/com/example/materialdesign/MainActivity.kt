39fb542b22da198a4d7b84423c52edcc1df14256
Marcelo-Stern <marcelo.silva776@etec.sp.gov.br>
2022-11-08 09:55:20 -0300

    Projeto Finalizado

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -2,16 +2,15 @@ package com.example.materialdesign
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
